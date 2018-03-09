c3d13107621e2bef2dd235ed95881dd5fc57f0b8
Татьяна Бахматович <tatianabahmatovic@student252-169.eduroam.uu.se>
2018-03-09 14:17:35 +0100

    added browser, redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,24 +6,23 @@ import FilterPosts from './../components/Blogs/BlogAction/FilterPosts';
 import { addBlog, removeBlog, filterBlogs } from '../redux/actions';
 
 const App = props => (
-            <div>
-                <h3>Posts</h3>
-                <FilterPosts
-                    filterBlogs={props.filterBlogs}
-                />
-                <Menu
-                    addBlog={props.addBlog}
-                    removeBlog={props.removeBlog}
-                    blogs={props.blogs}
-                />
-                <br/>
-                <div className='container-posts row'>
-                    {props.blogs.map((blog, index) => <Blog key={index} blog={blog} />)}
-                </div>
-            </div>
-        );
-//     }
-// }
+    <div>
+        <h3>Posts</h3>
+        <FilterPosts
+            filterBlogs={props.filterBlogs}
+        />
+        <Menu
+            addBlog={props.addBlog}
+            removeBlog={props.removeBlog}
+            blogs={props.blogs}
+        />
+        <br/>
+        <div className='container-posts row'>
+            {props.blogs.map((blog, index) => <Blog key={index} blog={blog} />)}
+        </div>
+    </div>
+);
+
 const mapStateToProps = state => ({
     blogs: state
 });
